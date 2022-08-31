1e2e1871f66da6bf7274e2e39da1d9df3acdd868
Rwix <96948593+RwixCoder@users.noreply.github.com>
2022-08-31 15:20:59 +0530

    Add files via upload

---
+++ t.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+struct dob {
+    int d,m,y;
+};
+struct details{
+    struct dob s;
+    char n[100];
+    char a[100];
+};
+int main(){
+    struct details user[5];
+    int i;
+    for(i=0;i<5;i++){
+    printf("enter name");
+    scanf("%s",&user[i].n);
+    printf("enter adress");
+    scanf("%s",&user[i].a);
+    printf("enter dob");
+    scanf("%d%d%d",&user[i].s.d,&user[i].s.m,&user[i].s.y);
+    }
+    for(i=0;i<5;i++) {
+    printf("\nName:%s",user[i].n);    
+    printf("\nAdress:%s",user[i].a);
+    printf("\ndob:%d %d %d",user[i].s.d,user[i].s.m,user[i].s.y);    
+    }
+    return 0;
+}
+
